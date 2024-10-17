ab467201682e1a5af567d88b136303547993c275
umamaheshy707 <163094785+umamaheshy707@users.noreply.github.com>
2024-10-17 16:05:07 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,13 @@
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+}
+
 variable "vpc_cidr" {
   type = string
   default = "10.0.0.0/16" # users can override
