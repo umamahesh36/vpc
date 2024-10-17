ff60ad460c09dee21f423f4daae14e501378a259
umamaheshy707 <163094785+umamaheshy707@users.noreply.github.com>
2024-10-17 16:02:19 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region = "us-east-1"  # Specify your desired AWS region
+  access_key = AKIA4HWJT7BX56TKQHYQ  # Optionally use variables for credentials
+  secret_key = l23PMxPSiCsdtVjBBRbom79+pd5HyrZlP+mm/Rp+
+}
