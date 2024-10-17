019e4032f9ff7fe7af1efb7a229c07fb2c0a820f
umamaheshy707 <163094785+umamaheshy707@users.noreply.github.com>
2024-10-17 16:04:24 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "us-east-1"  # Specify your desired AWS region
-  access_key = AKIA4HWJT7BX56TKQHYQ  # Optionally use variables for credentials
-  secret_key = l23PMxPSiCsdtVjBBRbom79+pd5HyrZlP+mm/Rp+
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
